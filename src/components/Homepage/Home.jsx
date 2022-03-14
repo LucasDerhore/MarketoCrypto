3a77ba8b52dbe1b921729255cfe794c1e9680507
vali <v.derhore@hotmail.fr>
2022-03-14 16:49:03 +0100

    index.js redux

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -3,13 +3,22 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+
 const { Title } = Typography;
 
 const Home = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
   return (
-    <Title level={2} className="heading">
-      Top 10 Best Cryptos moment
-    </Title>
+    <>
+      <div className="home-cryptos">
+        <Title level={2} className="heading">
+          Top 10 Best Cryptos moment
+        </Title>
+      </div>
+    </>
   );
 };
 
