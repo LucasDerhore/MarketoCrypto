ce4ddd16628e85411845a5c7dea079f0fa776e28
MainieriQuentin <quentin.mainieri@live.fr>
2022-03-14 16:05:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
+
+import {
+  Navbar,
+  Home,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
 
 const App = () => {
   console.log(process.env.REACT_APP_TOKENCOINRANKING);
@@ -6,8 +16,28 @@ const App = () => {
   console.log(process.env.REACT_APP_TOKENNFT);
 
   return (
-    <div>
-      <h1>Crypto Marketo</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="../Cryptocurrencies/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route
+                path="../CryptoDetails/cryptodetails"
+                element={<CryptoDetails />}
+              />
+              <Route path="../News/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
     </div>
   );
 };
