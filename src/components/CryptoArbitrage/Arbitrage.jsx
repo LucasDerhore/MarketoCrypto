8ab01a2603615e128d9640096307d2c8aaeb0d62
lucasd <derhore.lucas@gmail.com>
2022-03-14 09:53:48 +0100

    first commit

---
+++ src/components/CryptoArbitrage/Arbitrage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Arbitrage = () => {
+  return <div>Arbitrage</div>;
+};
+
+export default Arbitrage;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "../components/Navbar/Navbar";
+export { default as Cryptocurrencies } from "../components/Cryptocurrencies/Cryptocurrencies";
+export { default as CryptoDetails } from "../components/CryptoDetails/CryptoDetail";
+export { default as News } from "../components/News/News";
+export { default as Home } from "../components/Homepage/Home";
+export { default as CryptoArbitrage } from "../components/CryptoArbitrage/Arbitrage";
+export { default as LineChart } from "../components/Linechart/LineChart";
