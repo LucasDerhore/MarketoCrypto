d3419d8d220589a271adeba7694cfc3a0c7a0306
lucasd <derhore.lucas@gmail.com>
2022-03-14 15:49:40 +0100

    API Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const App = () => {
+  console.log(process.env.REACT_APP_TOKENCOINRANKING);
+  console.log(process.env.REACT_APP_TOKENBING);
+  console.log(process.env.REACT_APP_TOKENNFT);
+
   return (
     <div>
       <h1>Crypto Marketo</h1>
