79bd0783e45425f20744fd6bd9bff34f2bbeeeff
vali <v.derhore@hotmail.fr>
2022-03-14 15:59:56 +0100

    App.js routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Home,
+  News,
+  Nft,
+  LineChart,
+  Navbar,
+  Footer,
+} from "./components";
 
 const App = () => {
   console.log(process.env.REACT_APP_TOKENCOINRANKING);
@@ -6,8 +17,29 @@ const App = () => {
   console.log(process.env.REACT_APP_TOKENNFT);
 
   return (
-    <div>
-      <h1>Crypto Marketo</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <div className="routes">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/cryptocurrencies"
+              element={<Cryptocurrencies />}
+            />
+            <Route exact path="/cryptodetails" element={<CryptoDetails />} />
+            <Route exact path="/news" element={<News />} />
+            <Route exact path="/nft" element={<Nft />} />
+            <Route exact path="/linechart" element={<LineChart />} />
+          </Routes>
+        </div>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Title level={2} className="heading">
+      Top 10 Best Cryptos moment
+    </Title>
+  );
 };
 
 export default Home;
