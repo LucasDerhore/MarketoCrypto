1c4f2bd0c20781f1bdf23fb4ebbee5e2d7959b91
lucasd <derhore.lucas@gmail.com>
2022-03-15 16:01:51 +0100

    Footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Layout, Typography, Space } from "antd";
+import { Link } from "react-router-dom";
+
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <Typography.Title
+        level={5}
+        style={{ color: "white", textAlign: "center" }}
+      >
+        CryptoMarketo <br />
+        All rights reserverd
+      </Typography.Title>
+      <Space>
+        <Link to="/">Home</Link>
+        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        <Link to="/news">News</Link>
+        <Link to="/nft">NFT</Link>
+      </Space>
+    </div>
+  );
 };
 
 export default Footer;
