4e7148daf6541022fa32ca362eaac41f76a6506f
vali <v.derhore@hotmail.fr>
2022-03-25 11:46:06 +0100

    Navbar Respensive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import image from "../../images/cryptocurrency.png";
 import image_1 from "../../images/metamask.svg";
 import "../Navbar/Navbar.scss";
-import { Avatar } from "antd";
+import { Avatar, Typography } from "antd";
 import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <label for="toggle">☰</label>
+        <label for="toggle">
+          <Title lvl={3} className="nav-title">
+            <Avatar src={image} size="large" className="nav-logo" />
+            Marketo Crypto
+          </Title>
+          ☰
+        </label>
         <input type="checkbox" id="toggle"></input>
         <div className="nav-link">
-          <Avatar src={image} size="large" />
-          <Link to="/">Marketo Crypto</Link>
+          <Avatar src={image} size="large" className="nav-logo" />
+          <Title lvl={3} className="nav-title">
+            Marketo Crypto
+          </Title>
           <Link to="/">Home</Link>
           <Link to="/Cryptocurrencies">Cryptos</Link>
           <Link to="/News">News</Link>
